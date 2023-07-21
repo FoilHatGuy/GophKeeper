e224a07a70b363d6dabc474b8c0da0ba296fd2d6
Anton Aleynikov <46861008+FoilHatGuy@users.noreply.github.com>
2023-07-21 14:43:06 +0400

    Revert "5 configs"

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,24 +4,18 @@ import (
 	"fmt"
 
 	"gophKeeper/src/client/app"
-	"gophKeeper/src/client/cfg"
 )
 
-var (
+const (
 	buildVersion = "N/A"
 	buildDate    = "N/A"
 	buildCommit  = "N/A"
 )
 
 func main() {
-	config := cfg.New(
-		cfg.FromDefaults(),
-		cfg.FromJSON(),
-		cfg.FromFlags(),
-	)
 	fmt.Printf("buildVersion\t= %q\n", buildVersion)
 	fmt.Printf("buildDate\t= %q\n", buildDate)
 	fmt.Printf("buildCommit\t= %q\n", buildCommit)
 	fmt.Print("Wow, client is running!")
-	app.MainLoop(config)
+	app.MainLoop()
 }
